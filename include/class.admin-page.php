195740c7c7fc717ab0936bf1e95a1ee645744092
ded-zamkad <ded.zamkad@gmail.com>
2015-09-04 18:49:46 +0300

    Donate button change

--- include/class.admin-page.php
+++ include/class.admin-page.php
@@ -185,14 +185,10 @@ class WpFlickrEmbed_Admin_Page implements WPFlickrEmbed_Constants {
             <p>Want to help make this plugin even better? All donations are used to improve this plugin, so please donate $5, $10 or $20!</p>
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                 <input type="hidden" name="cmd" value="_s-xclick">
-                <input type="hidden" name="hosted_button_id" value="SZZWUQ5D4ZFZE">
+                <input type="hidden" name="hosted_button_id" value="HV859WV8D4QUN">
                 <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
                 <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
             </form>
-            <p>Or you could:</p>
-            <ul>
-                <li><a href="http://l.hiddentao.com/wp-flickr-embed-rate-it">Rate the plugin 5★ on WordPress.org</a></li>
-                <li><a href="http://l.hiddentao.com/wp-flickr-embed-blog-about-it">Blog about it &amp; link to the plugin page</a></li>
         <?php
     }
 
